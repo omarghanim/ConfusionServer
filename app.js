e54a38dc5307eb024d0ee0e75888b7c190b95917
omar ghanim <omarmghanim@gmail.com>
2020-07-21 00:10:25 +0200

    File Upload

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
 var config = require('./config');
+var uploadRouter = require("./routes/uploadRouter")
 
 var app = express();
 //
@@ -57,6 +58,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dishes',dishRouter);
 app.use('/promotions',promoRouter);
 app.use('/leaders',leaderRouter);
+app.use("/imageUpload",uploadRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
