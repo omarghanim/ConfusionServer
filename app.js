ef5e92dc8e0d93b0e66b9afa3d0498eb616a9921
omar ghanim <omarmghanim@gmail.com>
2020-07-22 09:10:57 +0200

    Editing Https Code

--- app.js
+++ app.js
@@ -10,22 +10,115 @@ var authenticate = require("./authenticate");
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var https = require('https');
+var http = require('http');
 var fs = require('fs');
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
 var config = require('./config');
 var uploadRouter = require("./routes/uploadRouter")
-
 var app = express();
-//
-// app.all('*',(req,res,next)=>{
-//   if(req.secure){
-//     next();
-//   }else{
-//     res.redirect("https://"+req.hostname+":"+app.get("secPort")+req.url);
-//   }
-// })
+var debug = require('debug')('confusionserver:server');
+
+app.all('*',(req,res,next)=>{
+  if(req.secure){
+    next();
+  }else{
+    res.redirect("https://"+req.hostname+":"+app.get("secPort")+req.url);
+  }
+})
+
+
+var port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+app.set('secPort',port+443)
+
+/**
+ * Create HTTP server.
+ */
+
+var server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+  //
+  var options={
+    key : fs.readFileSync(__dirname+"/bin/private.key"),
+    cert: fs.readFileSync(__dirname+"/bin/certificate.pem")
+  }
+  var secureServer = https.createServer(options,app);
+
+  secureServer.listen(app.get("secPort"),()=>{            //configure the secure server
+    console.log("Secure Server listening on port",app.get("secPort"));
+  });
+
+
+  secureServer.on('error', onError);
+  secureServer.on('listening', onListening);
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
 
 const mongoose =require("mongoose");
 const Dishes=require("./models/dishes");
@@ -75,6 +168,5 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.listen(3000);
 
 module.exports = app;
